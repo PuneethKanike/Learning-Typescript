026fbb4cd2df9a71240146bb7884040dffb2a1ae
Puneeth K <kanike.puneeth@gmail.com>
2024-10-07 11:44:38 +0530

    learnt about the type case

--- practice/app.js
+++ practice/app.js
@@ -1,27 +1,4 @@
 "use strict";
-//literals
-let directions;
-let responseCode;
-//these tells that the value you are going to asign to the variable within those values which you have specified
-//if u try to asign a any other value it will give error
-//eg
-//responseCode = 600
-//enums
-var SIze;
-(function (SIze) {
-    SIze[SIze["Small"] = 100] = "Small";
-    SIze[SIze["Medium"] = 200] = "Medium";
-    SIze[SIze["Large"] = 201] = "Large";
-})(SIze || (SIze = {}));
-var size = SIze.Small;
-if (size === SIze.Small) {
-}
-//string enums
-var Direction;
-(function (Direction) {
-    Direction["up"] = "UP";
-    Direction["down"] = "DOWN";
-    Direction["left"] = "LEFT";
-    Direction["right"] = "RIGHT";
-})(Direction || (Direction = {}));
-console.log(Direction.down);
+//type cast
+let x = 1;
+const res = x = "hello";
--- practice/app.ts
+++ practice/app.ts
@@ -1,13 +1,5 @@
-//any type if we dont want a type check
+//type cast
 
-let x:any = 1
+let x:unknown = 1;
 
-//unknown type when we dont know the type
-
-let y:unknown = 1
-
-if(typeof y === "number"){
-    const res = x + 1
-} else if(typeof y === "string"){
-    const res = y.length
-}
+const res = (x as string) = "hello"
