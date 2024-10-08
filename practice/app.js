cb8db86a12faa9a90c3626a136a2425e9dc0d75d
Puneeth K <kanike.puneeth@gmail.com>
2024-10-08 11:05:21 +0530

    learnt about types in functions

--- practice/app.js
+++ practice/app.js
@@ -1,4 +1,7 @@
 "use strict";
-//type cast
-let x = 1;
-const res = x = "hello";
+// types in functions
+function add(x, y) {
+    return x + y;
+}
+const res = add(1, 2);
+console.log(res);
--- practice/app.ts
+++ practice/app.ts
@@ -1,5 +1,16 @@
-//type cast
+// types in functions
 
-let x:unknown = 1;
+function add(x:number,y:number): number | string {  //we add number | string to return only from on of them
+    return x+y
+}
 
-const res = (x as string) = "hello"
+const res = add(1,2)
+console.log(res)
+
+
+
+function makeName (fname:string, lname:string, mname?:string){  // we added the ? mark fot the mname specifiing that that mname is a optional one
+    
+}
+
+makeName("puneeth","kanike")
